7776b550c80da84e8e034be499f279460b1174f8
Salisu Abubakar <salisbell404@gmail.com>
2024-09-03 13:20:36 +0100

    update

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
-import {thunk} from 'redux-thunk';
+import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
 
 const initialState = {};
 const middleWare = [thunk];
 
-export const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(...middleWare)));
+const composeEnhancers =
+  process.env.NODE_ENV === 'development'
+    ? composeWithDevTools(applyMiddleware(...middleWare))
+    : applyMiddleware(...middleWare);
+
+export const store = createStore(rootReducer, initialState, composeEnhancers);
